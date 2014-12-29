ca129c8dff57c70c652fa14958c7de2d5b4343eb
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2014-12-29 16:44:40 +0900

    create sleipnir spec example

---
+++ sampleSleipnirSpecsTest/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  sampleSleipnirSpecsTest
+//
+//  Created by KazuakiMATSUO on 12/29/14.
+//  Copyright (c) 2014 KazuakiMATSUO. All rights reserved.
+//
+
+import Foundation
+import Sleipnir
+
+Runner.run()
+
